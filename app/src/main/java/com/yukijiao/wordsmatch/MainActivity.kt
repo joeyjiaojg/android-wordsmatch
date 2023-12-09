7dd8ba898de506d042245eea991628163aefdd2f
Joey Jiao <joeyjiaojg@qq.com>
2023-12-09 12:25:36 +0800

    add Spinner to choose words list

--- app/src/main/java/com/yukijiao/wordsmatch/MainActivity.kt
+++ app/src/main/java/com/yukijiao/wordsmatch/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.yukijiao.wordsmatch
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.MultiAutoCompleteTextView
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private var filename = "words_alpha.txt";
+    private var lines = mutableListOf<String>();
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val inputStream: InputStream = assets.open("words_alpha.txt")
-        val lines = readInputStreamLines(inputStream)
+        val inputStream: InputStream = assets.open(filename)
+        lines = readInputStreamLines(inputStream).toMutableList()
+
+        val spinnerWordsList = findViewById<Spinner>(R.id.spinner_words_list);
+        spinnerWordsList.onItemSelectedListener = this
 
         val btnFilter = findViewById<Button>(R.id.btn_filter_word)
         btnFilter.setOnClickListener{
@@ -27,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun filterWords(lines:List<String>, reString: String) {
+    private fun filterWords(lines:List<String>, reString:String) {
         val output = findViewById<MultiAutoCompleteTextView>(R.id.output)
         output.text.clear()
         var reStr = reString
@@ -56,4 +68,13 @@ class MainActivity : AppCompatActivity() {
         return lines
     }
 
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        val spinnerWordsList = findViewById<Spinner>(R.id.spinner_words_list);
+        filename = spinnerWordsList.selectedItem.toString()
+        val inputStream: InputStream = assets.open(filename)
+        lines = readInputStreamLines(inputStream).toMutableList()    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
 }
